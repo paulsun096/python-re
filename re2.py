c011c0365dbb0e2aabd8d563c5d48b7c05bf082e
Paul Sun <paulsun096@gmail.com>
2021-04-13 13:36:35 -0400

    comment out compile

--- re2.py
+++ re2.py
@@ -1,6 +1,9 @@
 import re
 
-m=re.compile('\w+@\w+')
-m=re.search("paulsun096@gmail.com", "paulsun096@gmail.com")
-
+#m=re.compile('\w+@\w+')
+m=re.search('\w+@\w+', 'paulsun096@gmail.com')
 print(m)
+
+#bdays=re.compile('0-12+/+0-30+/+1996')
+bdays=re.search('\d+/+\d+/\d\d\d\d', 'sfd4/2/1996sdf')
+print(bdays)
