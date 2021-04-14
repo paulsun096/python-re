a7ebbe0a12f86cec4860b01b9d9795afbc2ef220
Paul Sun <paulsun096@gmail.com>
2021-04-13 23:02:19 -0400

    use shortcode {4} for \d\d\d\d

--- re2.py
+++ re2.py
@@ -5,10 +5,11 @@ m=re.search('\w+@\w+', 'paulsun096@gmail.com')
 print(m)
 
 #bdays=re.compile('0-12+/+0-30+/+1996')
-bdays=re.search('\d+/+\d+/\d\d\d\d', 'sfd4/2/1996sdf')
+bdays=re.search('\d+/+\d+/\d{4}', 'sfd4/2/1996sdf')
 print(bdays)
 
 #credit card number 
 num='3245324323423451'
 cc=re.search('\d{16}', num)
 print(cc)
+
