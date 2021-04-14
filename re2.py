1e8c29a9b81a5e8e94e56d1088a9964b2cf8ff27
Paul Sun <paulsun096@gmail.com>
2021-04-13 22:52:23 -0400

    Update re2.py

--- re2.py
+++ re2.py
@@ -7,3 +7,8 @@ print(m)
 #bdays=re.compile('0-12+/+0-30+/+1996')
 bdays=re.search('\d+/+\d+/\d\d\d\d', 'sfd4/2/1996sdf')
 print(bdays)
+
+#credit card number 
+num='3245324323423451'
+cc=re.search('\d{16}', num)
+print(cc)
